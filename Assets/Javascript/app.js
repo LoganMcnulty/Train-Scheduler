1e10c8e993960a2c79cc76d5c4517bafa63518b9
Logan McNulty <logan.mcnulty@zmfs.local>
2019-10-07 17:53:26 -0400

    times working, added a clear page button, background image, more CSS, OBX card. Need to add Remove Ferry

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -19,42 +19,38 @@
   var destination = "";
   var frequency = "";
   var startTime = "";
-  var nextArrival = "";
-  var minutesAway = 0;
+  var currentTime = moment();
+  var newCurrentTime = currentTime.format("hh:mm")
 
-// var todaysDate = new Date();
-  // console.log(todaysDate);
-      
 // Capture Button Click
   $("#add-train").on("click", function(event) {
-    event.preventDefault();
+      event.preventDefault();
 
     // Grabbed values from text boxes
       trainName = $("#trainName-input").val().trim();
       destination = $("#destination-input").val().trim();
       frequency = $("#frequency-input").val().trim();
       startTime = $("#startTime-input").val().trim();
-
+      
     // Code for handling the push
-      database.ref().push({
+      database.ref("/trains").push({
         trainName: trainName,
         destination: destination,
         frequency: frequency,
         startTime: startTime,
       });
+      document.getElementById("ferryForm").reset();
   });
   
-
-
 // Firebase watcher .on("child_added"
-  database.ref().on("child_added", function(snapshot) {
+  database.ref("/trains").on("child_added", function(snapshot) {
+
   // storing the snapshot.val() in a variable for convenience
     var newTrainRow = $("<tr>")
     var allTrains = snapshot.val();
 
   // Console.loging the last user's data
     console.log(allTrains);
-    console.log(allTrains.trainName);
 
     var newTrainName = $("<td>").text(allTrains.trainName);
     newTrainRow.append(newTrainName);
@@ -65,14 +61,51 @@
     var newTrainFrequency = $("<td>").text(allTrains.frequency);
     newTrainRow.append(newTrainFrequency);
 
-    var newTrainNextArrival = $("<td>").text("19:00");
+    var firstTimeConverted = moment(allTrains.startTime, "HH:mm").subtract(1, "years");
+    
+    var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
+
+    var timeRemainder = diffTime % allTrains.frequency;
+
+    var timeUntillTrain = allTrains.frequency - timeRemainder;
+
+    var nextTrain = moment().add(timeUntillTrain, "minutes");
+    var nextTrainHours = nextTrain.format("hh:mm")
+
+    var newTrainNextArrival = $("<td>").text(nextTrainHours);
     newTrainRow.append(newTrainNextArrival);
 
-    var newTrainMinutesAway = $("<td>").text("30");
+    var newTrainMinutesAway = $("<td>").text(timeUntillTrain);
     newTrainRow.append(newTrainMinutesAway);
 
-    $("#trainContainer").append(newTrainRow);
+    var removeTrainTD = $("<td>").attr("trainName", allTrains.trainName);
+    var removeTrainbutton = $("<button>").text("✓");
+    removeTrainbutton.addClass("checkbox");
+    removeTrainTD.append(removeTrainbutton);
+    newTrainRow.append(removeTrainTD);
 
-  }, function(errorObject) {
+    $("#trainContainer").append(newTrainRow);
+  }, 
+    function(errorObject) {
     console.log("Errors handled: " + errorObject.code);
+    }
+  );
+
+// REMOVE A FERRY FUNCTION
+  $(document).on("click",".checkbox", function() {
+    event.preventDefault();
+    console.log("test");
+
+    
+    // var trainRunning = false;
+    // database.ref("trains").push({
+    //   trainRunning: trainRunning,
+    // });
   });
+
+// CLEAR THE SCHEDULE FUNCTION
+$("#clearSchedule").on("click", function(event) {
+  database.ref("trains").remove();
+  setTimeout(window.location.reload(), 2000);
+});
+
